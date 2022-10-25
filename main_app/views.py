89c08214e7449a972b2c4fb1ebf44f15ac829bdd
mGrimley <mich@elgrimley.com>
2022-10-25 11:40:54 -0700

    Refactors About page

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>Hello world</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the Lego Collector</h1>')
+    return render(request, 'about.html')
